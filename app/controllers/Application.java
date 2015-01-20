4c92725ead56b295a91b253242a05f25ce21d89e
Danny4927 <danielknoell@gmx.de>
2015-01-20 12:42:27 +0100

    cleanup unnecessary imports and files

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,9 @@
 package controllers;
 
-import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.formdata.UserFormData;
 import views.html.index;
-import views.html.register;
 
-import static play.data.Form.form;
 
 public class Application extends Controller {
 
